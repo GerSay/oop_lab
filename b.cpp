edd3b939efe5c98fe580635e938133bbb3cb97b8
GerSay <kryt.master@yandex.ru>
2023-03-16 11:52:41 +0300

    1 lab, file b

--- b.cpp
+++ b.cpp
@@ -1,28 +1,30 @@
 #include <fstream>
-#include <string>
 
-int b()
-{
+int b() {
 	std::ifstream fin;
 	fin.open("C://1.txt", std::ios_base::in);
 
-	char c;
+	char prev_space = 0;
 	std::string str;
+	char c;
 	while (fin.get(c))
 		if (c == ' ')
-		{
-			str += ' ';
-			while (c == ' ')
-				fin >> c;
+			prev_space = 1;
+
+		else {
+			if (prev_space) {
+				str += ' ';
+				prev_space = 0;
+			}
+
 			str += c;
 		}
-		else
-			str += c;
 
 	fin.close();
 
 	std::ofstream fout;
-	fout.open("C://1.txt", std::ios_base::trunc);
+	fout.open("C://1.txt", std::ios_base::out);
+
 	fout << str;
 
 	fout.close();
